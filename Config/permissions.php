140970004162337159c1e80ddc7c87a4572c9373
William Verde <williamverde10@gmail.com>
2019-08-27 13:17:29 -0400

    add manage permision

--- Config/permissions.php
+++ Config/permissions.php
@@ -2,30 +2,35 @@
 
 return [
     'iquiz.polls' => [
+        'manage' => 'iquiz::polls.manage resource',
         'index' => 'iquiz::polls.list resource',
         'create' => 'iquiz::polls.create resource',
         'edit' => 'iquiz::polls.edit resource',
         'destroy' => 'iquiz::polls.destroy resource',
     ],
     'iquiz.questions' => [
+        'manage' => 'iquiz::questions.manage resource',
         'index' => 'iquiz::questions.list resource',
         'create' => 'iquiz::questions.create resource',
         'edit' => 'iquiz::questions.edit resource',
         'destroy' => 'iquiz::questions.destroy resource',
     ],
     'iquiz.answers' => [
+        'manage' => 'iquiz::answer.manage resource',
         'index' => 'iquiz::answers.list resource',
         'create' => 'iquiz::answers.create resource',
         'edit' => 'iquiz::answers.edit resource',
         'destroy' => 'iquiz::answers.destroy resource',
     ],
     'iquiz.userquestionanswers' => [
+        'manage' => 'iquiz::userquestionanswers.manage resource',
         'index' => 'iquiz::userquestionanswers.list resource',
         'create' => 'iquiz::userquestionanswers.create resource',
         'edit' => 'iquiz::userquestionanswers.edit resource',
         'destroy' => 'iquiz::userquestionanswers.destroy resource',
     ],
     'iquiz.userpolls' => [
+        'manage' => 'iquiz::userpolls.manage resource',
         'index' => 'iquiz::userpolls.list resource',
         'create' => 'iquiz::userpolls.create resource',
         'edit' => 'iquiz::userpolls.edit resource',
